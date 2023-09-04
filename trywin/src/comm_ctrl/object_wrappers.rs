0380769382d0227a1ef409af8d7b11b4fd25fa37
Todd Fleming <tbfleming@gmail.com>
2023-09-04 00:57:47 -0400

    Create safer callbacks

--- trywin/src/comm_ctrl/object_wrappers.rs
+++ trywin/src/comm_ctrl/object_wrappers.rs
@@ -3,9 +3,18 @@
 // Many wrappers live in submodules to prevent accidental access to
 // the interior; access must be through unsafe raw()
 
+use std::{
+    cell::Cell,
+    panic::{catch_unwind, AssertUnwindSafe},
+    process::abort,
+};
 use windows::{
     core,
-    Win32::{Foundation::*, Graphics::Gdi::*, UI::WindowsAndMessaging::*},
+    Win32::{
+        Foundation::*,
+        Graphics::Gdi::*,
+        UI::{Shell::*, WindowsAndMessaging::*},
+    },
 };
 
 use super::Error;
@@ -43,6 +52,211 @@ mod raw_hwnd {
     }
 }
 pub use raw_hwnd::RawHwnd;
+
+mod owned_window {
+    use super::*;
+
+    pub struct OwnedWindow(HWND);
+
+    impl OwnedWindow {
+        /// # Safety
+        ///
+        /// Caller must ensure that hwnd is valid and not null.
+        /// This takes ownership and will destroy the window
+        /// when it is dropped using `[DestroyWindow]`.
+        pub unsafe fn new(hwnd: HWND) -> Self {
+            Self(hwnd)
+        }
+    }
+
+    impl Raw<HWND> for OwnedWindow {
+        // Safety: see Raw::raw()
+        unsafe fn raw(&self) -> HWND {
+            self.0
+        }
+    }
+
+    impl Drop for OwnedWindow {
+        fn drop(&mut self) {
+            // Safety: self.0 is valid
+            unsafe {
+                let _ = DestroyWindow(self.0);
+            }
+        }
+    }
+}
+pub use owned_window::*;
+
+pub mod window_proc {
+    use super::*;
+
+    pub trait WindowProc {
+        /// Safety:
+        /// * Caller is a window procedure that is currently handling a message.
+        ///   It is running in the same thread which created the HWND.
+        /// * Caller is providing a valid message.
+        /// * Caller sets `commctrl` to true iff the window procedure is a subclass
+        ///   procedure of a Windows Common Control Library control.
+        /// * Caller ensures that hwnd is valid and not null at the beginning of the call.
+        /// * Caller ensures that `&self` is valid for the duration of the call.
+        /// * Trait implementer should call `default` for any unhandled messages.
+        /// * It is OK for the trait implementer to call user-provided callbacks. They
+        ///   may destroy the HWND directly or indirectly, e.g. by destroying
+        ///   a parent HWND. Both caller and implementer need to safely handle this.
+        /// * Caller must not pass an invalid HWND to `default`.
+        unsafe fn wndproc<'a>(
+            &'a self,
+            commctrl: bool,
+            hwnd: HWND,
+            message: u32,
+            wparam: WPARAM,
+            lparam: LPARAM,
+            default: impl FnOnce(HWND, u32, WPARAM, LPARAM) -> LRESULT + 'a,
+        ) -> LRESULT;
+    }
+
+    pub struct StaticWndprocState<T> {
+        window_proc: T,
+        entry_count: Cell<u32>,
+        destroy_this: Cell<bool>,
+    }
+
+    impl<T> From<T> for StaticWndprocState<T> {
+        fn from(window_proc: T) -> Self {
+            Self {
+                window_proc,
+                entry_count: Cell::new(0),
+                destroy_this: Cell::new(false),
+            }
+        }
+    }
+
+    /// Safety:
+    /// * Let `p` be a `*mut StaticWndprocState<T>` obtained from
+    ///   `[Box::into_raw]`. `static_wndproc` owns `p` and will eventually
+    ///   release it using `drop(Box::from_raw(p))`. `[Box::into_raw]` must
+    ///   have been called on the same thread which created HWND.
+    /// * `CREATESTRUCTW::lpCreateParams` must be `p`; it can't be null.
+    /// * `GWLP_USERDATA` must either be null or be `p`.
+    /// * Must only be called by the Windows API.
+    /// * The Windows API guarantees that it will only call this function in the
+    ///   same thread that created the HWND.
+    pub unsafe extern "system" fn static_wndproc<T: WindowProc + 'static>(
+        handle: HWND,
+        message: u32,
+        wparam: WPARAM,
+        lparam: LPARAM,
+    ) -> LRESULT {
+        // Get p or immediately return if it's null.
+        let p: *const StaticWndprocState<T>;
+        if message == WM_NCCREATE {
+            p = (*(lparam.0 as *const CREATESTRUCTW)).lpCreateParams
+                as *const StaticWndprocState<T>;
+            SetWindowLongPtrW(handle, GWLP_USERDATA, p as isize);
+        } else {
+            p = GetWindowLongPtrW(handle, GWLP_USERDATA) as *const StaticWndprocState<T>;
+            if p.is_null() {
+                return DefWindowProcW(handle, message, wparam, lparam);
+            }
+        }
+
+        // Track recursion depth
+        let Some(c) = (*p).entry_count.get().checked_add(1) else {
+            abort();
+        };
+        (*p).entry_count.set(c);
+
+        // Call the callback
+        let res = catch_unwind({
+            let p = AssertUnwindSafe(p);
+            move || {
+                (**p).window_proc.wndproc(
+                    false,
+                    handle,
+                    message,
+                    wparam,
+                    lparam,
+                    |hwnd, message, wparam, lparam| DefWindowProcW(hwnd, message, wparam, lparam),
+                )
+            }
+        });
+
+        if message == WM_NCDESTROY || res.is_err() {
+            // Schedule p destruction and prevent further calls to wndproc
+            (*p).destroy_this.set(true);
+            SetWindowLongPtrW(handle, GWLP_USERDATA, 0);
+        }
+
+        // Track recursion depth
+        (*p).entry_count.set((*p).entry_count.get() - 1);
+
+        // Destroy p if scheduled and we're the last call
+        if (*p).entry_count.get() == 0 && (*p).destroy_this.get() {
+            drop(Box::from_raw(p as *mut StaticWndprocState<T>));
+        }
+
+        res.unwrap_or(LRESULT(0))
+    }
+
+    /// Safety:
+    /// * Must be registered as subclass 0.
+    /// * Let `p` be a `*mut StaticWndprocState<T>` obtained from
+    ///   `[Box::into_raw]`. `static_subclass_wndproc` owns `p` and will eventually
+    ///   release it using `drop(Box::from_raw(p))`. `[Box::into_raw]` must
+    ///   have been called on the same thread which created HWND.
+    /// * dwrefdata must be `p`. It can't be null.
+    /// * Must only be called by the Windows API.
+    /// * The Windows API guarantees that it will only call this function in the
+    ///   same thread that created the HWND.
+    pub unsafe extern "system" fn static_subclass_wndproc<T: WindowProc + 'static>(
+        handle: HWND,
+        message: u32,
+        wparam: WPARAM,
+        lparam: LPARAM,
+        _uidsubclass: usize,
+        dwrefdata: usize,
+    ) -> LRESULT {
+        let p = dwrefdata as *const StaticWndprocState<T>;
+
+        // Track recursion depth
+        let Some(c) = (*p).entry_count.get().checked_add(1) else {
+            abort();
+        };
+        (*p).entry_count.set(c);
+
+        // Call the callback
+        let res = catch_unwind({
+            let p = AssertUnwindSafe(p);
+            move || {
+                (**p).window_proc.wndproc(
+                    false,
+                    handle,
+                    message,
+                    wparam,
+                    lparam,
+                    |hwnd, message, wparam, lparam| DefSubclassProc(hwnd, message, wparam, lparam),
+                )
+            }
+        });
+
+        if message == WM_NCDESTROY || res.is_err() {
+            // Schedule p destruction and prevent further calls to wndproc
+            (*p).destroy_this.set(true);
+            RemoveWindowSubclass(handle, Some(static_subclass_wndproc::<T>), 0);
+        }
+
+        // Track recursion depth
+        (*p).entry_count.set((*p).entry_count.get() - 1);
+
+        // Destroy p if scheduled and we're the last call
+        if (*p).entry_count.get() == 0 && (*p).destroy_this.get() {
+            drop(Box::from_raw(p as *mut StaticWndprocState<T>));
+        }
+
+        res.unwrap_or(LRESULT(0))
+    }
+}
+pub use window_proc::*;
 
 /// (x, y, w, h)
 pub fn get_window_rect(hwnd: &impl Raw<HWND>) -> Result<(i32, i32, i32, i32), Error> {
